e6d9181c36109af268648a7707879407d56f5676
SabinShadow <mjucarrillo.1990@gmail.com>
2021-07-20 19:42:07 -0600

    antes de los cambios del sensei

---
+++ src/models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_FACTOR = 10;
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        index: {unique:true}
+    },
+    birth_date: Date,
+    is_active: {
+        type: Boolean,
+        default: false
+    },
+    gender: {
+        type: String,
+        enum: ['male','female','other']
+    }
+}, {collection: 'users', timestamps: true});
+
+UserSchema.pre('save', function (next){
+    let user = this;
+    if (!user.isModified('passsword')) { return next() }
+    bcrypt.genSalt( SALT_FACTOR, function(err, salt) {
+      if(err) return next(err);
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if(err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  });
+
+const User = mongoose.model('users', UserSchema);
+
+module.exports = { User };
---
+++ src/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const { errors } = require('celebrate')
+const mongoose = require('mongoose')
+const routes = require('./routes')
+
+const {db: {url}, port } = require('./config')
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(morgan('dev'));
+app.use('/api/v1', routes);
+app.use(errors())
+console.log('config ->', url,port)
+
+mongoose.connect(url, {
+    useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error',(error) => console.log('FAILED to connect to mongo', error));
+db.once('open', () =>{
+    console.log('SUPERMARKET API RUN...')
+    
+}
+);
+app.listen(port, () => {
+    console.log(`Server start on port ${port}`)
+})
+
+
